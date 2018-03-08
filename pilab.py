3fc51cb2a95a135a2be06db614a17b39b2672eee
Rishi Yadav <23rishiyadav@gmail.com>
2018-03-08 20:09:06 +0530

    Update pilab.py
    Sytax Updated

--- pilab.py
+++ pilab.py
@@ -1,6 +1,6 @@
 #pip install Flask
 #export FLASK_APP=pilab.py
-#python -m flask run--host=0.0.0.0  for intranet
+#python -m flask run --host=0.0.0.0  for intranet
 #http://0.0.0.0:5000/on
 #http://0.0.0.0:5000/off
 
